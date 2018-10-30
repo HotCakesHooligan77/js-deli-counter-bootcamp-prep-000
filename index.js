a4f89334bce1c9da858ecfb7a8b2a28f583579e8
Charity Jackson <charitylennjackson@yahoo.com>
2018-10-30 17:27:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function currentLine(line){
   if(!line.length){
     return "The line is currently empty."
  }
- const numberAndNames =[];
+ const numberAndNames = [];
  
-  for (let i =0, l=line.length; i<l;i++){
+  for (let i = 0, l = line.length; i<l; i++){
     numberAndNames.push('${i+1}. ${line[i]}')
   }
 }
