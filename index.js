5469415609068a4c72b25a41999218f11d214b98
Charity Jackson <charitylennjackson@yahoo.com>
2018-10-30 17:41:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 var katzDeliLine = [];
 
 function currentLine(katzDeliLine){
-  if(!line.length){
+  if(!katzDeliLine.length){
     return "The line is currently empty."
  }
  
  
-  for (let i = 0, l = line.length; i<l; i++){
+  for (let i = 0, l = katzDeliLine.length; i<l; i++){
     katzDeliLine.push('${i+1}. ${line[i]}')
   }
 }
 
-function nowServing(line){
-  if(!line.length){
+function nowServing(katzDeliLine){
+  if(!katzDeliLine.length){
     return "There is nobody waiting to be served!"
   }
   return 'Currently serving ${line.shift()}.'
@@ -20,7 +20,7 @@ function nowServing(line){
 
 function takeANumber(katzDeliLine, name){
   
-    line.push(name);
+    katzDeliLine.push(name);
   
-  return 'Welcome, ${katzDeliLine}. You are number 1 in line.';
+  return 'Welcome, ${katzDeliLine}. You are number  in line.';
 }
